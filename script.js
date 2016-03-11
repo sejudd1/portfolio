56f6ebcf084b3a6b4cd805df180845df4575a4ab
Steve Judd <sejudd1@github.com>
2016-03-11 14:48:59 -0800

    gets ajax to weather.com working

--- script.js
+++ script.js
@@ -32,24 +32,33 @@ $( document ).ready( function(){
 		$(".col-lg-4 #mem").addClass("rotate3")
 	})
 })
-
+console.log('P')
 $.ajax({
-	url: "http://api.wunderground.com/api/b325241e6545289b/conditions/q/CA/Culver_City.json",
-	type: 'GET',
-	data: {
-		zipcode: 90232
-	},
-	dataType: 'jsonp',
-	success: function( data ) {
-		console.log('are we there yet?');
-		$( "#weatherTemp").html( "<strong>" + data + "<strong> degrees");
-	},
-	
-});
+	method: 'GET',
+	url: 'http://api.wunderground.com/api/b325241e6545289b/conditions/q/CA/Culver_City.json',
+	data: JSON.stringify({response:$('#weatherTemp').val() }),
+	dataType : "jsonp"
+}).then(
+	function( response ) {
+		console.log('are we there yet?', response );
+		// var location = JSON.stringify({response:$('display_location','full', 'local_time_rfc822')});
+  // 		var temp_f = JSON.stringify({response:$('current_observation','temp_f','weather').val() });
+  		// var $weatherTemp = $(response).filter('#weatherTemp');
+  		var location = response.current_observation.display_location.full;
+  		var temp_f = response.current_observation.temp_f;
+  		var time = response.current_observation.observation_time_rfc822;
+  		var forecast = response.current_observation.weather;
+		$( '#weatherTime' ).html( 'Today is '  + time + '.');
+		$( '#weatherTemp' ).html( "Currently it's " + temp_f + 'F' + ' in ' + location + '.');
+		$( '#forecast' ).html( "Today's forecast: " + forecast);
+		
+	}	
+)
 
-var api_endpoint = "http://api.wunderground.com/api/b325241e6545289b/conditions/q/CA/Culver_City.json"
-$.get(api_endpoint)
- .success(renderWeather)
- .fail(function(){
- 	alert("Whoa, major problemo with the api!")
- });
+
+// var api_endpoint = "http://api.wunderground.com/api/b325241e6545289b/conditions/q/CA/Culver_City.json"
+// $.get(api_endpoint)
+//  .success(renderWeather)
+//  .fail(function(){
+//  	alert("Whoa, major problemo with the api!")
+//  });
--- server.js
+++ server.js
@@ -6,3 +6,14 @@ app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
 });
+
+// sample api route
+app.get('/api/b325241e6545289b/conditions/q/CA/Culver_City.json', function(req, res) {
+        // if there is an error retrieving, send the error. 
+        // nothing after res.send(err) will execute
+        if (err)
+            res.send(err);
+
+        res.json(data); // return data in JSON format
+
+});
